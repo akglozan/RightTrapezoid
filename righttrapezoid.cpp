418936e2688aeda9318fc844a48ecd853b0bb2f9
akglozan <ozanakgul2001@gmail.com>
2023-05-16 12:15:26 +0200

    added main.cpp

--- righttrapezoid.cpp
+++ righttrapezoid.cpp
@@ -1,6 +1,76 @@
-#ifndef RIGHTTRAP__
-#define RIGHTTRAP__
 
 
+#include "polygon.h"
+#include "righttrapezoid.h"
 
-#endif
+float RightTrap::Side()
+{
+	return 0.0f;
+}
+
+RightTrap::RightTrap()
+{
+	height = 0.0f;
+	topSide = 0.0f;
+	bottomSide = 0.0f;
+
+	cout << "Default Right Trapezoid" << endl;
+
+}
+
+RightTrap::RightTrap(RightTrap& X)
+{
+	cout << "Copy Right Trapezoid" << endl;
+	Init(X);
+}
+
+RightTrap::~RightTrap()
+{
+	cout << "Right Trapezoid Destructor" << endl;
+	Reset();
+}
+
+RightTrap& RightTrap::operator=(RightTrap& X)
+{
+	Reset();
+	Init(X);
+	cout << "Operator = Right Trapezoid" << endl;
+	
+	return *this;
+}
+
+bool RightTrap::operator==(RightTrap& X)
+{
+	return false;// da guardare
+}
+
+void RightTrap::Init()
+{
+}
+
+void RightTrap::Init(RightTrap& X)
+{
+	Reset();
+	//da finire
+}
+
+void RightTrap::Reset()
+{
+	cout << "Reset Right Trapezoid" << endl;
+	height = 0.;
+	topSide = 0.;
+	bottomSide = 0.;
+
+}
+
+void RightTrap::Dump()
+{
+	Polygon::Dump();
+
+	cout << "Height = " << height << endl;
+	cout << "Top Side = " << topSide << endl;
+	cout << "Bottom Side = " << bottomSide << endl;
+	//need to output oblique side
+	
+
+}
--- righttrapezoid.h
+++ righttrapezoid.h
@@ -1,1 +1,62 @@
-#pragma once
+#ifndef RIGHTTRAP__
+#define RIGHTTRAP__
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+
+#include "polygon.h"
+
+
+class RightTrap :public Polygon
+{
+private:
+	
+
+protected:
+	float height;
+	float topSide;
+	float bottomSide;
+	float Side();
+
+	
+
+
+public:
+	//Default Constructor
+	RightTrap();
+
+	//Copy Constructor
+	RightTrap(RightTrap& X);
+
+	//Destructor
+	virtual ~RightTrap();
+
+	//Assignment overloading
+	RightTrap& operator = (RightTrap& X);
+
+	//Comparison overloading
+	bool operator==(RightTrap& X);
+
+	//Default initialization
+	void Init();
+
+	//Copy initialization
+	void Init(RightTrap& X);
+
+	//Delete object
+	void Reset();
+
+	//Object printout for diagnostic
+	void Dump();
+
+};
+
+
+
+
+
+#endif
